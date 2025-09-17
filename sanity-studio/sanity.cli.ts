841d6eb501e1ddd167dc79d918256bafb12c05e9
Aditya <pandeyaditya032002@gmail.com>
2025-09-17 12:16:06 +0530

    adds appid

--- sanity-studio/sanity.cli.ts
+++ sanity-studio/sanity.cli.ts
@@ -5,6 +5,9 @@ export default defineCliConfig({
     projectId: 'w77riyw5',
     dataset: 'production'
   },
+  deployment: {
+    appId: 'aq502ruilhlxzf0brwqcvmqa',
+  },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
