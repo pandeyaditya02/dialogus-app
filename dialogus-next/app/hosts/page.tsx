dbac0d21b0bf04947630c4b817fc7c333946d367
Aditya Pandey <pandeyaditya032002@gmail.com>
2025-11-16 17:12:42 +0530

    adds new hosts section

---
+++ dialogus-next/app/hosts/page.tsx
@@ -0,0 +1,10 @@
+import Speakers from "../components/Speakers";
+
+export default function HostsPage() {
+  return (
+    <main className="pt-24 bg-black">
+      <Speakers />
+    </main>
+  );
+}
+
